766a8f8863764157fb39b2c711ca5a8e5bbc2ab0
fernandosenacruz <fernandosenacruz@gmail.com>
2022-11-21 16:03:21 -0300

    Modificador final

---
+++ aula042/Constants.java
@@ -0,0 +1,7 @@
+package aula042;
+
+public final class Constants {
+  public static final String GOOGLE = "https://www.google.com/";
+  public static final String WORLDWIDE = "Palmeiras não tem mundial!";
+
+}
---
+++ aula042/Final.java
@@ -0,0 +1,7 @@
+package aula042;
+
+public class Final {
+  public static void main(String[] args) {
+    System.out.println(Constants.WORLDWIDE);
+  }  
+}
